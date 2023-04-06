df0ce708e52e2ae51b9ccf81b20d5427df8caa88
aaalis <topovoe@yandex.ru>
2023-04-06 23:06:21 +0800

    Добавьте файлы проекта.

---
+++ TeacherApp/TeacherApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using TeacherApp.ViewModels;
+using TeacherApp.Views;
+using Xamarin.Forms;
+
+namespace TeacherApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ TeacherApp/TeacherApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TeacherApp.Models;
+using TeacherApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TeacherApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
